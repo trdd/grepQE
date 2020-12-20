745a03de7955087463175e6e8b0f32c9d1c953ac
Tobias Ritschl <t.ritschel@posteo.de>
2020-12-20 18:49:13 +0100

    small typo

--- grepqe.py
+++ grepqe.py
@@ -8,7 +8,7 @@ import matplotlib.pyplot as plt
 import os.path as path
 
 parser = argparse.ArgumentParser('Quickly get some cell info from several qe calcs')
-parser.add_argument('--pre', metavar=p, type=str, nargs=1,* default='', help='directory prefix to look for outfiles')
+parser.add_argument('--pre', metavar=p, type=str, nargs=1, default='', help='directory prefix to look for outfiles')
 parser.add_argument('--file', metavar=fn, type=str, nargs=1, default='*.out', help='directory prefix to look for outfiles')
 parser.add_argument('--plot', action='store_true')
 parser
@@ -22,6 +22,3 @@ for d in dirs:
     for f in outf:
         try:
             out = io.read(f, format='espresso-out')
-            print(ostr.format(curr, *out.cell.cellpar())
-        except Exception as e:
-            pass
