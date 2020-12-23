adc1f20b57fade202d27975e8a48cfe52c2972c6
Tobias Ritschl <t.ritschel@posteo.de>
2020-12-23 23:55:30 +0100

    new unis option

--- grepqe.py
+++ grepqe.py
@@ -1,6 +1,7 @@
 #!/bin/env python
 
 from ase import io
+from ase import units
 import sys
 import argparse
 import glob
@@ -8,11 +9,13 @@ import matplotlib.pyplot as plt
 import os.path as path
 import numpy as np
 
+# command line arguments
 parser = argparse.ArgumentParser('Quickly get some cell info from several qe calcs')
 parser.add_argument('-p', '--pre', type=str, default='', help='directory prefix to look for outfiles')
 parser.add_argument('-f', '--file', type=str, default='*.out', help='outfile wildcard')
 parser.add_argument('--plot', action='store_true')
 parser.add_argument('-m', '--mode', default='cell', choices=['cell', 'to_cif'])
+parser.add_argument('--units', default='eV', choices=['eV', 'Ry'])
 args = parser.parse_args()
 dirs = glob.glob(args.pre+'*/')
 vals = [d.lstrip(args.pre).rstrip('/') for d in dirs]
@@ -20,14 +23,18 @@ vals = [float(t) if t.isdigit() else t for t in vals]
 ind = np.argsort(vals)
 
 if args.mode == 'cell':
-    ostr = 7*'{:10.6} '
-    print(ostr.format('# '+args.pre, 'a', 'b', 'c', 'al', 'be', 'ga'))
+    if args.units == 'eV':
+        fac = 1.
+    elif args.units == 'Ry':
+        fac = units.create_units('2006')['Ry']   #CODATA 2006 is hard coded in ase.
+    ostr = '{:<10.6} '+6*'{:>10.6} '+'{:>20.15}'
+    print(ostr.format('# '+args.pre, 'a', 'b', 'c', 'al', 'be', 'ga', 'energy'))
     for i in ind:
         outf = glob.glob(path.join(dirs[i],args.file))
         for f in outf:
             try:
                 out = io.read(f, format='espresso-out')
-                print(ostr.format(vals[i], *out.cell.cellpar()))
+                print(ostr.format(vals[i], *out.cell.cellpar(), out.get_total_energy()/fac))
             except Exception as e:
                 pass
 
